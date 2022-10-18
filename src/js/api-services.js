d8e0a64175740a4b1fb2edcc8a343514903429e2
Maksym Bernatskyi <bernasik007@gmail.com>
2022-10-18 23:17:58 +0300

    commit 4

---
+++ src/js/api-services.js
@@ -0,0 +1,40 @@
+import axios from 'axios/dist/axios.min.js';
+const API_KEY = '30579822-7048314e53034861817646057';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export default class apiService {
+  constructor() {
+    this.name = '';
+    this.API_KEY = API_KEY;
+    this.BASE_URL = BASE_URL;
+    this.page = 1;
+    }
+    
+    async fetchArticles(page) {
+    try {
+      const response = await axios.get(
+        `${this.BASE_URL}?key=${this.API_KEY}&q=${this.name}&image_type=photo&orientation=horizontal&safesearch=true&page=${page}&per_page=40`,
+      );
+      const articles = await response.data;
+      return articles;
+        } catch (error) {
+      console.error(error);
+        }
+    }
+
+    incrementPage() {
+    this.page += 1;
+    }
+
+    resetPage() {
+    this.page = 1;
+    }
+
+    get searchName() {
+    return this.name;
+    }
+
+    set searchName(newName) {
+    this.name = newName;
+    }
+}
---
+++ src/js/app.js
@@ -0,0 +1,6 @@
+import apiService from './api-services';
+import compiledTemplate from '../card.hbs';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
